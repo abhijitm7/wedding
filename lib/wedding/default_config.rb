c5413ee6676a23d90969b463d0d0d71c32d7ef31
abhijitm7 <33685894+abhijitm7@users.noreply.github.com>
2017-11-15 20:14:45 +0530

    Update default_config.rb

--- lib/wedding/default_config.rb
+++ lib/wedding/default_config.rb
@@ -2,21 +2,21 @@ module Wedding
   module DefaultConfig
     def self.options
       {
-        :groom_name => "Jai",
-        :groom_about => "Programmer, Traveller, Photographer, Biker, SlideShare Engineer",
-        :groom_occupation => "Hacker at SlideShare",
-        :groom_email => "jaipandya@gmail.com",
-        :bride_name => "Prerita",
-        :bride_about => "Banker, Traveller, Dancer, Painter, Dreamer",
-        :bride_occupation => "Asst. Manager at Bank of Baroda",
-        :bride_email => "preritayadav@gmail.com",
+        :groom_name => "Abhijit",
+        :groom_about => "Traveller, Photographer, Biker, SlideShare Engineer",
+        :groom_occupation => "System Administrator",
+        :groom_email => "abhijitmore7@gmail.com",
+        :bride_name => "Pratiksha",
+        :bride_about => "Traveller, Dancer, Painter, Dreamer",
+        :bride_occupation => "",
+        :bride_email => "",
         :location => "+26° 52' 57.3024\", +75° 48' 24.75\"",
         :event_schedule => [
-          " 7:00 pm Barat starts from Home",
-          " 8:00 pm Barat reaches venue",
-          "12:00 am Phere"
+          " 11:00 pm Barat starts from Home",
+          " 12:00 pm Barat reaches venue",
+          "1:00 am Phere"
         ],
-        :date => "2013-11-29"
+        :date => "2017-12-03"
       }
     end
   end
